54aaa0b3654ebbbf35a45ef894e319d48dc89583
kimgwon <92065911+kimgwon@users.noreply.github.com>
2022-07-12 10:30:04 +0900

    fragment layout 진행중

--- app/src/main/java/com/example/naenaeng/ui/home/AddIngredientFragment.kt
+++ app/src/main/java/com/example/naenaeng/ui/home/AddIngredientFragment.kt
@@ -1,4 +1,13 @@
 package com.example.naenaeng.ui.home
 
-class AddIngredientFragment {
+import com.example.naenaeng.R
+import com.example.naenaeng.base.BaseFragment
+import com.example.naenaeng.databinding.FragmentAddIngredientBinding
+
+class AddIngredientFragment: BaseFragment<FragmentAddIngredientBinding>(R.layout.fragment_add_ingredient) {
+    override fun initDataBinding() {
+        super.initDataBinding()
+
+        binding.imageView3.clipToOutline = true
+    }
 }
--- app/src/main/java/com/example/naenaeng/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/naenaeng/ui/home/HomeFragment.kt
@@ -1,10 +1,5 @@
 package com.example.naenaeng.ui.home
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.naenaeng.R
 import com.example.naenaeng.base.BaseFragment
 import com.example.naenaeng.databinding.FragmentHomeBinding
