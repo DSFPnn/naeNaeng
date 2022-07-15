b02be9558ba8872ec9fe0209a65c1e91d85e8a41
mmihye <smilehye910@naver.com>
2022-07-16 01:39:28 +0900

    로그인 기능 추가

--- app/src/main/java/com/example/naenaeng/ui/info/LoginFragment.kt
+++ app/src/main/java/com/example/naenaeng/ui/info/LoginFragment.kt
@@ -1,16 +1,28 @@
 package com.example.naenaeng.ui.info
 
+import android.widget.Toast
 import com.example.naenaeng.MainActivity
 import com.example.naenaeng.R
 import com.example.naenaeng.base.BaseFragment
 import com.example.naenaeng.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
+    private lateinit var auth: FirebaseAuth
 
     override fun initStartView() {
         super.initStartView()
         (activity as MainActivity).setToolbar("none2")
+        auth = Firebase.auth
+
+        //로그인 되어있는지 확인
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            //navController.navigate(R.id.action_loginFragment_to_homeFragment)
+        }
     }
 
     override fun initDataBinding() {
@@ -27,6 +39,34 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login
             navController.navigate(R.id.action_loginFragment_to_homeFragment)
         }
 
+        binding.btnRegister.setOnClickListener {
+            var isGoToLogin = true
+            val email = binding.btnIngredientName.text.toString()
+            val password = binding.btnIngredientLife.text.toString()
+
+            // 유효성 검사
+            if (email.isEmpty()) {
+                Toast.makeText(context, "이메일을 입력해주세요.", Toast.LENGTH_LONG).show()
+                isGoToLogin = false
+            }
+            if (password.isEmpty()) {
+                Toast.makeText(context, "비밀번호를 입력해주세요.", Toast.LENGTH_LONG).show()
+                isGoToLogin = false
+            }
+
+            if(isGoToLogin){
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(context, "로그인 성공",Toast.LENGTH_SHORT).show()
+                            navController.navigate(R.id.action_loginFragment_to_homeFragment)
+                        } else {
+                            Toast.makeText(context, "로그인 실패",Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+            }
+        }
 
     }
 }
