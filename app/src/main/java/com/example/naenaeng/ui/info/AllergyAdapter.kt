e02f20aaa013c9998468edf7bf5ff2033ccc0cb4
kimgwon <92065911+kimgwon@users.noreply.github.com>
2022-07-26 11:35:52 +0900

    allergyAdapter에 clickListener 추가

--- app/src/main/java/com/example/naenaeng/ui/info/AllergyAdapter.kt
+++ app/src/main/java/com/example/naenaeng/ui/info/AllergyAdapter.kt
@@ -2,12 +2,18 @@ package com.example.naenaeng.ui.info
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.CompoundButton
 import androidx.recyclerview.widget.RecyclerView
 import com.example.naenaeng.databinding.AllergyItemViewBinding
 import com.example.naenaeng.model.Allergy
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class AllergyAdapter(itemList: MutableList<Allergy>)
     : RecyclerView.Adapter<AllergyAdapter.ViewHolder>(){
+    private val db = Firebase.firestore
+    private var allergyDatas = mutableListOf<String>()
+
     var itemList: MutableList<Allergy> = itemList
         set(value) {
             field = value
@@ -17,7 +23,7 @@ class AllergyAdapter(itemList: MutableList<Allergy>)
     inner class ViewHolder(itemViewBinding: AllergyItemViewBinding)
         :RecyclerView.ViewHolder(itemViewBinding.root){
         var allergy = itemViewBinding.tvAllergy
-        //var allergyCheck = itemViewBinding.checkBox
+        var allergyCheck = itemViewBinding.checkBox
     }
 
     override fun onCreateViewHolder(
@@ -34,8 +40,22 @@ class AllergyAdapter(itemList: MutableList<Allergy>)
     }
 
     override fun onBindViewHolder(holder: AllergyAdapter.ViewHolder, position: Int) {
+
         holder.allergy.text = itemList[position].type
         //holder.allergyCheck.isChecked = itemList[position].allergy_check==1
+
+        //사용자별 알러지 DB에 저장
+        holder.allergyCheck.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                allergyDatas.add(holder.allergy.text.toString())
+                db.collection("users").document(prefs.getString("email", "null"))
+                    .update("allergy", allergyDatas)
+            } else {
+                allergyDatas.remove(holder.allergy.text.toString())
+                db.collection("users").document(prefs.getString("email", "null"))
+                    .update("allergy", allergyDatas)
+            }
+        }
     }
 
     override fun getItemCount(): Int = itemList.size
